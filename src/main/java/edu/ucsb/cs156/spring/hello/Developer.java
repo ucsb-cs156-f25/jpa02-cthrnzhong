30b905a191f9c57a38648ab0863a229fdfe4cf4e
Catherine Zhong <cthrnzhong@gmail.com>
2025-10-08 19:40:37 -0700

    updated names

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -35,12 +35,12 @@ public class Developer {
     
     public static Team getTeam() {
         Team team = new Team("f25-14");
-        team.addMember("Pratima");
-        team.addMember("Keyi");
-        team.addMember("Catherine");
-        team.addMember("Logan");
-        team.addMember("Daniel");
-        team.addMember("Fernando");
+        team.addMember("PRATIMA REDDY");
+        team.addMember("KEYI");
+        team.addMember("CATHERINE");
+        team.addMember("LOGAN CHRISTOPHER");
+        team.addMember("DANIEL JUNHYUK");
+        team.addMember("FERNANDO");
         return team;
     }
 }
--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -41,11 +41,11 @@ public class DeveloperTest {
     @Test
     public void getTeam_returns_team_with_correct_members(){
         Team t = Developer.getTeam();
-        assertTrue(t.getMembers().contains("Catherine"),"Team should contain Catherine");
-        assertTrue(t.getMembers().contains("Daniel"),"Team should contain Daniel");
-        assertTrue(t.getMembers().contains("Fernando"),"Team should contain Fernando");
-        assertTrue(t.getMembers().contains("Pratima"),"Team should contain Pratima");
-        assertTrue(t.getMembers().contains("Keyi"),"Team should contain Keyi");
-        assertTrue(t.getMembers().contains("Logan"),"Team should contain Logan");
+        assertTrue(t.getMembers().contains("CATHERINE"),"Team should contain Catherine");
+        assertTrue(t.getMembers().contains("DANIEL JUNHYUK"),"Team should contain Daniel");
+        assertTrue(t.getMembers().contains("FERNANDO"),"Team should contain Fernando");
+        assertTrue(t.getMembers().contains("PRATIMA REDDY"),"Team should contain Pratima");
+        assertTrue(t.getMembers().contains("KEYI"),"Team should contain Keyi");
+        assertTrue(t.getMembers().contains("LOGAN CHRISTOPHER"),"Team should contain Logan");
     }
 }
